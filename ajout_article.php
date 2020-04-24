34acc122749b73c9c72272813aaeab3832411026
cyrielletouton <cyrielle.t@hotmail.fr>
2020-04-24 11:01:05 +0200

    upload images articles

--- ajout_article.php
+++ ajout_article.php
@@ -2,13 +2,13 @@
 // Aller chercher la liste de toutes les catégories par ordre alphabétique
 // Nécessaire AVANT de pouvoir afficher le formulaire
 // On se connecte à la base
-require_once('inc/connect.php') ;
+require_once('inc/connect.php');
 
-    // On écrit la requête SQL
+// On écrit la requête SQL
 $sql = 'SELECT * FROM `categories` ORDER BY `name` ASC;';
 
 // Pas de variables, donc utilisation de la méthode query
-$query = $db->query($sql) ;
+$query = $db->query($sql);
 
 // on récupère les données
 $categories = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -19,30 +19,79 @@ $categories = $query->fetchAll(PDO::FETCH_ASSOC);
 // 2ème partie du traitement, lui affecter la/les catégorie(s)
 
 // On vérifie que le formulaire a été envoyé
-if(isset($_POST) && !empty($_POST)){
+if (isset($_POST) && !empty($_POST)) {
     // On a un formulaire envoyé
     // On vérifie que tout est bien rempli
     require_once('inc/lib.php');
-    if(verifForm($_POST, ['titre', 'contenu', 'categories'])){
+    if (verifForm($_POST, ['titre', 'contenu', 'categories'])) {
         // Le formulaire est complet, on peut créer l'article
         // On récupère les valeurs et on nettoie
         $titre = strip_tags($_POST['titre']);
         $contenu = strip_tags($_POST['contenu'], '<div><p><h1><h2><img><strong>');
 
-        // On est déjà connectés
-        // On écrit la requête SQL
-        $sql = 'INSERT INTO `articles` (`title`, `content`, `users_id`) VALUES (:titre, :contenu, :userid);' ;
-        
-        // On prépare la requête
-        $query = $db->prepare($sql);
+        // On vérifie si on a une image
+        if (isset($_FILES) && !empty($_FILES)) {
+            // On vérifie que tous les fichiers attendus sont envoyés
+            // Erreur 4 = pas de fichier (image non obligatoire)
+            if (isset($_FILES['image']) && !empty($_FILES['image']) && $_FILES['image']['error'] !=4) {
+                // On récupère les données
+                $image = $_FILES['image'];
 
-        // On injecte les valeurs dans la requête
-        $query->bindValue(':titre', $titre, PDO::PARAM_STR) ;
-        $query->bindValue(':contenu', $contenu, PDO::PARAM_STR) ;
-        $query->bindValue(':userid', 1, PDO::PARAM_INT) ;
+                // On vérifie si le transfert s'est mal déroulé 'error' = 1
+                if ($image['error'] != 0) {
+                    echo 'Une erreur s\'est produite';
+                    die;
+                }
 
-        // On exécute la requête
-        $query->execute();
+                // On limite aux images png et jpg (jpeg aussi)
+                $types = ['image/png', 'image/jpeg'];
+
+                // On vérifie si le type du fichier est absent de la liste
+                if (!in_array($image['type'], $types)) {
+                    echo "Le type de fichier doit être une image jpg ou png";
+                    die;
+                }
+
+                // On veut limiter la taille à 1Mo max
+                if ($image['size'] > 1048576) {
+                    echo 'Le fichier est trop volumineux (1Mo maxi)';
+                    die;
+                }
+
+                // Le transfert s'est bien déroulé, on déplace l'image temporaire après lui avoir généré un nouveau nom
+                // Générer un nom pour le fichier -> nom + extension
+                // On récupère l'extension de notre fichier
+                $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
+
+                // On génère un nom  "aléatoire"
+                $nom = md5(uniqid()) . '.' . $extension;
+
+                // On génère le nom complet -> nom et chemin complet vers le dossier de destination
+                $nomComplet = __DIR__ . '/uploads/' . $nom;
+
+                // On déplace le fichier
+                if (!move_uploaded_file($image['tmp_name'], $nomComplet)) {
+                    echo "Le fichier n'a pas été copié !";
+                    die;
+                }
+            }
+        }
+
+            // On est déjà connectés
+            // On écrit la requête SQL
+            $sql = 'INSERT INTO `articles` (`title`, `content`, `featured_image`, `users_id`) VALUES (:titre, :contenu, :image, :userid);';
+
+            // On prépare la requête
+            $query = $db->prepare($sql);
+
+            // On injecte les valeurs dans la requête
+            $query->bindValue(':titre', $titre, PDO::PARAM_STR);
+            $query->bindValue(':contenu', $contenu, PDO::PARAM_STR);
+            $query->bindValue(':userid', 1, PDO::PARAM_INT);
+            $query->bindValue(':image', $nom, PDO::PARAM_STR);
+
+            // On exécute la requête
+            $query->execute();
 
         // On récupère l'id de l'article nouvellement créé
         $idArticle = $db->lastInsertId();
@@ -51,10 +100,10 @@ if(isset($_POST) && !empty($_POST)){
         $categories = $_POST['categories'];
 
         // On ajoute les catégories
-        foreach($categories as $categorie){
+        foreach ($categories as $categorie) {
             // On écrit la requête
             $sql = 'INSERT INTO `articles_categories`(`articles_id`, `categories_id`) VALUES (:idArticle, :idCategorie);';
-        
+
             // On prépare la requête
             $query = $db->prepare($sql);
 
@@ -65,13 +114,12 @@ if(isset($_POST) && !empty($_POST)){
             // On exécute la requête
             $query->execute();
         }
-        
+
         // On redirige vers une autre page (liste des catégories par exemple)
         header('Location: index.php');
 
         // On se déconnecte de la base
-        require_once ('inc/close.php');
-
+        require_once('inc/close.php');
     } else {
         echo "Le formulaire doit être rempli complètement";
     }
@@ -82,14 +130,16 @@ if(isset($_POST) && !empty($_POST)){
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajouter un article</title>
 </head>
+
 <body>
     <h1>Ajouter un article</h1>
-    <form method="post">
+    <form method="post" enctype="multipart/form-data">
         <div>
             <label for="titre">Titre de l'article : </label>
             <input type="text" id="titre" name="titre">
@@ -98,8 +148,14 @@ if(isset($_POST) && !empty($_POST)){
             <label for="contenu">Contenu : </label>
             <textarea name="contenu" id="contenu" cols="30" rows="10"></textarea>
         </div>
+        <h2>Image</h2>
+        <!-- ATTENTION enctype obligatoire quand un champ "file" est utilisé -->
+        <div>
+            <label for="image">Image : </label>
+            <input type="file" name="image" id="image">
+        </div>
         <h2>Catégories</h2>
-        <?php foreach($categories as $categorie): ?>
+        <?php foreach ($categories as $categorie) : ?>
             <div>
                 <input type="checkbox" name="categories[]" id="cat_<?= $categorie['id'] ?>" value="<?= $categorie['id'] ?>">
                 <label for="cat_<?= $categorie['id'] ?>"> <?= $categorie['name'] ?> </label>
@@ -107,6 +163,7 @@ if(isset($_POST) && !empty($_POST)){
         <?php endforeach; ?>
         <button>Ajouter l'article</button>
     </form>
-    
+
 </body>
+
 </html>
