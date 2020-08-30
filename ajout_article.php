bc4cd4c91c67fe7503eef68af7adb7bbe56d23d5
cyrielletouton <cyrielle.t@hotmail.fr>
2020-08-30 18:48:19 +0200

    Add article / localisation

--- ajout_article.php
+++ ajout_article.php
@@ -149,10 +149,33 @@ require_once('inc/close.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/journal/bootstrap.min.css" rel="stylesheet" integrity="sha384-vjBZc/DqIqR687k5rf6bUQ6IVSOxQUi9TcwtvULstA7+YGi//g3oT2qkh8W1Drx9" crossorigin="anonymous">
     <title>Ajouter un article</title>
 </head>
 
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="index.php">CyTravel</i></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_articles.php">Admin articles <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_users.php">Admin users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_localisation.php">Admin localisation</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
     <h1>Ajouter un article</h1>
 
     <?php
@@ -180,13 +203,17 @@ require_once('inc/close.php');
         </div>
         <h2>Localisations</h2>
         <?php foreach ($localisations as $localisation) : ?>
-            <div>
-                <input type="checkbox" name="localisations[]" id="loc_<?= $localisation['id'] ?>" value="<?= $localisation['id'] ?>">
-                <label for="loc_<?= $localisation['id'] ?>"> <?= $localisation['name'] ?> </label>
-            </div>
+        <div>
+            <input type="checkbox" name="localisations[]" id="loc_<?= $localisation['id'] ?>" value="<?= $localisation['id'] ?>">
+            <label for="loc_<?= $localisation['id'] ?>"> <?= $localisation['name'] ?> </label>
+        </div>
         <?php endforeach; ?>
-        <button>Ajouter l'article</button>
+        <button class="btn btn-primary">Ajouter l'article</button>
     </form>
+    <br>
+    <footer style="background-color:#EB6864">
+        <p style="color:white" class="text-center"> Copyright © 2020 - Cyrielle.T - Juste un test en PHP ;)</p>
+    </footer>
 
 </body>
 
--- ajout_localisation.php
+++ ajout_localisation.php
@@ -2,10 +2,10 @@
 
 // Ici on traite le formulaire
 // A-t-on un $_POST
-if (isset($_POST) && !empty($_POST)){
+if (isset($_POST) && !empty($_POST)) {
     // On vérifie si tous les champs du formulaire sont remplis
     require_once('inc/lib.php');    // Correspond a : if(isset($_POST['nom']) && !empty($_POST['nom'])){
-    if(verifForm($_POST, ['nom'])){    
+    if (verifForm($_POST, ['nom'])) {
         // On récupère la valeur saisie dans chaque champ
         // Pour éviter les failles XSS (Cross Site Scripting)
         // Méthode 1 : Enlever les balises html
@@ -24,7 +24,7 @@ if (isset($_POST) && !empty($_POST)){
         $query = $db->prepare($sql);
 
         // On injecte les valeurs dans la requête
-        $query->bindValue(':nom', $nom, PDO::PARAM_STR) ;
+        $query->bindValue(':nom', $nom, PDO::PARAM_STR);
 
         // On exécute la requête
         $query->execute();
@@ -34,29 +34,57 @@ if (isset($_POST) && !empty($_POST)){
 
         // On redirige vers la liste des catégories
         header('Location: admin_localisation.php');
-        }
-    } else {
-        echo "Attention il faut entrer un nom";
     }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/journal/bootstrap.min.css" rel="stylesheet" integrity="sha384-vjBZc/DqIqR687k5rf6bUQ6IVSOxQUi9TcwtvULstA7+YGi//g3oT2qkh8W1Drx9" crossorigin="anonymous">
     <title>Ajouter une localisation</title>
 </head>
+
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="index.php">CyTravel</i></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_articles.php">Admin articles <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_users.php">Admin users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_localisation.php">Admin localisation</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
     <h1>Ajouter une localisation</h1>
     <form method="post">
         <div>
             <label for="nom">Nom de la localisation :</label>
             <input type="text" id="nom" name="nom">
         </div>
-        <button>Ajouter la localisation</button>
+        <button class="btn btn-primary">Ajouter la localisation</button>
     </form>
-    
+    <br>
+    <footer style="background-color:#EB6864">
+        <p style="color:white" class="text-center"> Copyright © 2020 - Cyrielle.T - Juste un test en PHP ;)</p>
+    </footer>
+
 </body>
+
 </html>
