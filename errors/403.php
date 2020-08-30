f2176ee8b1fe73efadecf584d624c882adf7dc46
cyrielletouton <cyrielle.t@hotmail.fr>
2020-08-30 18:45:42 +0200

    Errors pages

--- errors/403.php
+++ errors/403.php
@@ -1,13 +1,46 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/journal/bootstrap.min.css" rel="stylesheet" integrity="sha384-vjBZc/DqIqR687k5rf6bUQ6IVSOxQUi9TcwtvULstA7+YGi//g3oT2qkh8W1Drx9" crossorigin="anonymous">
     <title>Erreur 403 - Interdit</title>
 </head>
+
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="index.php">CyTravel</i></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="admin_articles.php">Admin articles <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_users.php">Admin users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_localisation.php">Admin localisation</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
     <h1>Vous n'avez pas accès à cette page</h1>
     <p>La ressource est protégée, vous n'avez pas les permissions suffisantes pour y accéder.</p>
-    <a href="/blog-ct">Retour à l'accueil</a>
+    <p><strong>Veuillez vous connecter en tant qu'administrateur.</strong></p>
+    <p><a href="/blog-ct">Retour à l'accueil</a></p>
+    <img src="img/close.jpg">
+    
+    <footer style="background-color:#EB6864">
+        <p style="color:white" class="text-center"> Copyright © 2020 - Cyrielle.T - Juste un test en PHP ;)</p>
+    </footer>
+
 </body>
+
 </html>
--- errors/404.php
+++ errors/404.php
@@ -1,13 +1,45 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/journal/bootstrap.min.css" rel="stylesheet" integrity="sha384-vjBZc/DqIqR687k5rf6bUQ6IVSOxQUi9TcwtvULstA7+YGi//g3oT2qkh8W1Drx9" crossorigin="anonymous">
     <title>Erreur 404</title>
 </head>
+
 <body>
-    <h1>Je n'ai pas trouvé ce que vous cherchez</h1>
-    <p>La ressource recherchée est introuvable pour le moment, veuillez réessayer ultérieurement, ou pas.</p>
-    <a href="/blog-ct">Retour à l'accueil</a>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="index.php">CyTravel</i></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="admin_articles.php">Admin articles <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_users.php">Admin users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_localisation.php">Admin localisation</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <h1>Perdu(e)..?</h1>
+    <p>La ressource recherchée est introuvable. Cette page n'existe pas.</p>
+    <p><a href="/blog-ct">Retour à l'accueil</a></p>
+    <img src="img/lost.png">
+
+    <footer style="background-color:#EB6864">
+        <p style="color:white" class="text-center"> Copyright © 2020 - Cyrielle.T - Juste un test en PHP ;)</p>
+    </footer>
+    
 </body>
+
 </html>
